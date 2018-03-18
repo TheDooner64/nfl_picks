3be26e1c0ea94e558860cb6167f5fe8adec0a748
TheDooner64 <robert.muldoon.1@gmail.com>
2018-03-18 15:13:32 -0400

    Add basic SQLAlchemy implementation and Team table

--- nfl_picks/__init__.py
+++ nfl_picks/__init__.py
@@ -2,9 +2,15 @@
 from __future__ import unicode_literals
 
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 # App
 app = Flask(__name__)
+
+# Database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Views
 from . import views  # NOQA
--- nfl_picks/models.py
+++ nfl_picks/models.py
@@ -1,2 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+import datetime
+
+from nfl_picks import db
+
+
+class Team(db.Model):
+    __tablename__ = 'team'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    conference = db.Column(db.String(100), nullable=False)
+    division = db.Column(db.String(100), nullable=False)
+    espn_name = db.Column(db.String(100), unique=True, nullable=False)
+    oddsshark_name = db.Column(db.String(100), unique=True)
+    date_added = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
+    date_updated = db.Column(db.DateTime(), default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+
+
+def insert_or_update_team(team_data):
+    team_name = team_data.get('team_name')
+    existing_record = get_team(team_name)
+
+    if existing_record:
+        db.session.execute(db.update(Team).where(Team.id == existing_record.id).values(team_data))
+    else:
+        db.session.execute(db.insert(Team, team_data))
+
+    db.session.commit()
+
+    return get_team(team_name)
+
+
+def get_team(team_name):
+    return Team.query.filter_by(name=team_name).first()
+
+
+def get_teams():
+    return Team.query.all()
